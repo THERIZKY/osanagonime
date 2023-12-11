a007494bfabae4128bdf43c98c869012d5a5852c
uwaissUno <149289804+uwaissUno@users.noreply.github.com>
2023-12-11 17:06:17 +0700

    Edit side bar

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,22 +2,22 @@
 import { Fragment, useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import Sidebar from "@/components/Layouts/SideBars";
+import Sidebar from "@/components/Layouts/Sidebar";
 
 export default function AdminLayout({
-	children, // will be a page or nested layout
+  children, // will be a page or nested layout
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	useEffect(() => {
-		AOS.init();
-	}, []);
-	return (
-		<Fragment>
-			{/* <Sidebar /> */}
-			<div className="w-full min-h-screen pl-[21rem] bg-slate-600">
-				{children}
-			</div>
-		</Fragment>
-	);
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  return (
+    <Fragment>
+      <Sidebar />
+      <div className="w-full min-h-screen pl-[21rem] bg-slate-600">
+        {children}
+      </div>
+    </Fragment>
+  );
 }
