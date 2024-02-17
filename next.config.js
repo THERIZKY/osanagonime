2ff23072fbb67c29879d860fee08155a4e76943c
RIZU <68154002+THERIZKY@users.noreply.github.com>
2024-02-18 00:13:40 +0700

    nambahin speedInsight

--- next.config.js
+++ next.config.js
@@ -20,6 +20,12 @@ const nextConfig = {
 				port: "",
 				pathname: "/**",
 			},
+			{
+				protocol: "https",
+				hostname: "daisyui.com",
+				port: "",
+				pathname: "/**",
+			},
 		],
 	},
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import "animate.css";
 import { SessionProvider } from "next-auth/react";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 			<body className={inter.className}>
 				<SessionProvider>{children}</SessionProvider>
 				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	);
