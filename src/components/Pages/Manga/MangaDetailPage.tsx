8ad9a2b30b65d93e588f3a8b11a87c45d0070e8e
uwaissUno <149289804+uwaissUno@users.noreply.github.com>
2024-02-18 22:38:45 +0700

    fix : fixing image background at manga detail page

--- src/components/Pages/Manga/MangaDetailPage.tsx
+++ src/components/Pages/Manga/MangaDetailPage.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 
 const MangaDetailPage = (dataManga: any) => {
+	console.log(dataManga.cover);
+	const urlBg = `url(${dataManga.cover})`
 	return (
 		<>
 			{dataManga && dataManga.chapters && (
-				<div className="max-h-full" style={{backgroundImage : `url(${dataManga.cover})`}}>
+				<div className="max-h-full" style={{backgroundImage : urlBg}}>
 					<div className={`hero min-h-[100%] overflow-hidden bg-slate-800/75`}>
 						<div className="hero-content flex-col lg:flex-row">
 							<Image width={200} height={200} src={dataManga.cover} className="max-w-sm rounded-lg shadow-2xl" alt="" />
