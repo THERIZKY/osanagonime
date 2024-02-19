3390bc18b763f4b458173e15b5a7d59c3a39d73c
uwaissUno <149289804+uwaissUno@users.noreply.github.com>
2024-02-20 06:27:01 +0700

    fix : fising overflow content at manga detail page

--- src/components/Pages/Manga/MangaDetailPage.tsx
+++ src/components/Pages/Manga/MangaDetailPage.tsx
@@ -12,9 +12,9 @@ const MangaDetailPage = (dataManga: any) => {
 					
 					
 					
-					<div className={`hero min-h-[100%] overflow-hidden `}>
-						<Image width={200} height={200} src={dataManga.cover} className="w-full h-full md:max-h-[40rem] lg:max-h-[21rem] object-cover pt-0 mt-0" alt="" />
-						<div className="hero-content w-full flex-col lg:flex-row bg-slate-800/75 ">
+					<div className={`hero min-h-[50%] overflow-hidden`}>
+						<Image width={400} height={400} src={dataManga.cover} className="w-full h-full md:max-h-[40rem] lg:max-h-[21rem] object-cover pt-0 mt-0" alt="" />
+						<div className="hero-content h-full max-w-full flex-col lg:flex-row bg-slate-800/75 ">
 							<Image width={200} height={200} src={dataManga.cover} className="max-w-sm rounded-lg shadow-2xl" alt="" />
 							<div>
 								<h1 className="text-5xl font-bold">{dataManga.mangaTitle}</h1>
