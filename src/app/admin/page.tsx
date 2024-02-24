8400032dddd255bd5eaa389b04f8ad85e447e741
RIZU <68154002+THERIZKY@users.noreply.github.com>
2024-02-24 15:45:39 +0700

    Updating To Server Comp

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,20 +1,19 @@
-"use client";
 import { Fragment } from "react";
 
 /*******************Admin Component********************/
 export default function Admin() {
-	// return (
-	// 	<Fragment>
-	// 		<div className="content flex items-center w-full h-12 ">
-	// 			<h3 className="text-white uppercase font-bold">Dashboard</h3>
-	// 		</div>
-	// 		<div className="tab-container justify-around flex ">
-	// 			<div className="w-1/5 h-[150px] bg-orange-200">test</div>
-	// 			<div className="w-1/5 h-[150px] bg-orange-200">test</div>
-	// 			<div className="w-1/5 h-[150px] bg-orange-200">
-	// 				<button className="btn btn-primary">Test</button>
-	// 			</div>
-	// 		</div>
-	// 	</Fragment>
-	// );
+	return (
+		<Fragment>
+			<div className="content flex items-center w-full h-12 ">
+				<h3 className="text-white uppercase font-bold">Dashboard</h3>
+			</div>
+			<div className="tab-container justify-around flex ">
+				<div className="w-1/5 h-[150px] bg-orange-200">test</div>
+				<div className="w-1/5 h-[150px] bg-orange-200">test</div>
+				<div className="w-1/5 h-[150px] bg-orange-200">
+					<button className="btn btn-primary">Test</button>
+				</div>
+			</div>
+		</Fragment>
+	);
 }
